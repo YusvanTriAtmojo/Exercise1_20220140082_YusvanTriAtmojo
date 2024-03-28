1a0593d7e587d1647c35d4a9c12a15c3b4f9b368
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-03-28 21:21:37 +0700

    my first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil(){
+        alert("Pikaaaaa Chuuuu ⚡️⚡️⚡️")
+    }
+
+    function show() {
+        alert("Pemilik Pikachu bernama Ash atau Satoshi")
+    }
+}
